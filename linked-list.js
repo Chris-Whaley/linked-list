c5cef217a3840894c16fceaa2abce5e09ca6e699
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2025-03-12 20:12:52 -0400

    restructuring to classes

--- linked-list.js
+++ linked-list.js
@@ -1,22 +1,116 @@
-function LinkedList(params) {
-  let head = null;
-  const linkedListEntries = {};
-  this.value = null;
-  this.nextNode = null;
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-  function append(value) {
-    if (this.value == null) {
-      head = value;
-      linkedListEntries[value] = this.nextNode;
-      this.value = value;
+  prepend(value) {
+    if (this.head === null) {
+      const newHead = new Node(value);
+      this.head = newHead;
     } else {
-      linkedListEntries[this.value] = value;
-      linkedListEntries[value] = this.nextNode;
-      this.value = value;
+      let currentHead = this.head;
+      const newHead = new Node(value, currentHead);
+      this.head = newHead;
     }
   }
 
-  return { linkedListEntries, append };
+  append(value) {
+    if (this.head === null) {
+      this.prepend(value);
+    } else {
+      let nodes = this.head;
+      while (nodes.nextNode != null) nodes = nodes.nextNode;
+      nodes.nextNode = new Node(value);
+    }
+  }
+
+  head() {
+    return this.head;
+  }
+
+  tail() {
+    let nodes = this.head;
+    while (nodes.nextNode != null) nodes = nodes.nextNode;
+    return nodes.value;
+  }
+
+  size() {
+    let counter = 1;
+    let nodes = this.head;
+    while (nodes.nextNode != null) {
+      nodes = nodes.nextNode;
+      counter += 1;
+    }
+    return counter;
+  }
+
+  at(index) {
+    let counter = 0;
+    let nodes = this.head;
+    if (index == 0) return this.head;
+    if (index > this.size()) return console.error();
+    while (counter < index) {
+      nodes = nodes.nextNode;
+      counter += 1;
+    }
+    return nodes;
+  }
+
+  pop() {
+    let current = this.head;
+    let previous;
+    if (this.size() == 0) return;
+    else {
+      while (current.nextNode !== null) {
+        previous = current;
+        current = current.nextNode;
+      }
+      previous.nextNode = null;
+    }
+  }
+
+  contains(value) {
+    let nodes = this.head;
+    let numberOfNodes = this.size();
+    while (numberOfNodes > 0) {
+      if (nodes.value == value) {
+        return true;
+      } else {
+        nodes = nodes.nextNode;
+        numberOfNodes--;
+      }
+    }
+    return false;
+  }
+
+  find(value) {
+    let nodes = this.head;
+    let numberOfNodes = this.size();
+    for (let index = 0; index < numberOfNodes; index++) {
+      if (nodes.value == value) {
+        return index;
+      } else {
+        nodes = nodes.nextNode;
+      }
+    }
+    return null;
+  }
+
+  toString() {
+    let nodes = this.head;
+    let outputString = "";
+    while (nodes.nextNode != null) {
+      outputString += `(${nodes.value}) -> `;
+      nodes = nodes.nextNode;
+    }
+    outputString += `(${nodes.value}) -> null`;
+    return outputString;
+  }
 }
 
-export { LinkedList };
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-import { LinkedList } from "./linked-list.js";
+import LinkedList from "./linked-list.js";
 
 // example uses class syntax - adjust as necessary
 const list = new LinkedList();
 
-list.append("dog");
+list.append("bird");
+list.prepend("alpacac");
 list.append("cat");
-list.append("parrot");
-list.append("hamster");
-list.append("snake");
-list.append("turtle");
+console.log(list.tail());
+console.log(list.size());
+console.log(list.at(0));
+console.log(list.at(1));
+console.log(list.at(4));
+list.pop();
+console.log(list.contains("bird"));
+console.log(list.contains("saosifoaid"));
+console.log(list.find("bird"));
+console.log(list.toString());
